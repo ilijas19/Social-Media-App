c2ce359e146c19fc617c8c400a45c37b86fd2e03
ilijas19 <ilijagocic19@gmail.com>
2024-11-28 18:39:23 +0100

    additional features

--- app.js
+++ app.js
@@ -7,20 +7,40 @@ const socketIO = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+//packages
 const socket = socketIO(server);
 const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
 //DB
 const connectDb = require("./db/connectDb");
 //ROUTERS
 const authRouter = require("./routes/authRoutes");
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+const profileRouter = require("./routes/profileRoutes");
+
 //MIDDLEWARES
 const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
 //APP
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/post", postRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/profile", profileRouter);
 
 app.use(notFound);
 app.use(errorHandler);
